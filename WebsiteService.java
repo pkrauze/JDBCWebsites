f421671dcf75fd605c691ad809e545d35b83bb21
Paweł Krauze <pawel.krauze@netguru.pl>
2017-11-13 11:30:00 +0100

    fix mavis project and change package names

---
+++ WebsiteService.java
@@ -0,0 +1,43 @@
+package com.websites.service;
+
+import java.util.List;
+
+import com.websites.domain.Website;
+
+public interface WebsiteService {
+	public int addWebsite(Website website);
+	
+	public int addAllWebsitesFromList(List<Website> websites);
+	
+	public List<Website> getAllWebsites();
+	
+	public int updateWebsite(Website website, String domain, String description, int page_rank, int page_authority, int domain_authority);
+	
+	public int updateWebsiteDomain(Website website, String domain);
+	
+	public int updateWebsiteDescription(Website website, String description);
+	
+	public int updateWebsitePageRank(Website website, int page_rank);
+	
+	public int updateWebsitePageAuthority(Website website, int page_authority);
+	
+	public int updateWebsiteDomainAuthority(Website website, int domain_authority);
+	
+	public int removeAllWebsites();
+	
+	public int removeWebsiteById(long id);
+	
+	public int removeWebsiteByDomain(String domain);
+	
+	public Website findById(long id);
+	
+	public Website findByDomain(String domain);
+	
+	public Website findByDescription(String description);
+	
+	public Website findByPageRank(int page_rank);
+	
+	public Website findByPageAuthority(int page_authority);
+	
+	public Website findByDomainAuthority(int domain_authority);
+}
